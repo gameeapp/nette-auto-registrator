9fffa39c59e93cd7f907db6712c030222ebfee7b
Pavel Janda <me@paveljanda.com>
2019-12-18 10:42:21 +0100

    src, README

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # nette-auto-registrator
-Automatically register all found classes into DIC
+
+Using this extension, you don't have to list all classes in neon config. This extension will automatically register all classes for you.
+
+## Installation
+
+```sh
+composer require gamee/nette-auto-registrator
+```
+
+## Usage
+
+### config.neon:
+
+```
+extensions:
+	autoRegistrator: AL\AutoRegistrator\DI\AutoRegistratorExtension
+
+autoRegistrator:
+	skipDirs:
+		- Extension
+	scanDirs:
+		- %appDir%
+	skipFilesPatterns:
+		- '/Exception\.php$/'
+	skipClasses:
+		- App\Foo\Bar
+		- App\MyBoomCreator
+```
+
+### Yes sir, you can use it also in another extension:
+
+```php
+declare(strict_types=1);
+
+namespace MyProject\Foo\DI;
+
+use Gamee\AutoRegistrator\DI\AutoRegistratorExtension;
+use Nette\DI\CompilerExtension;
+
+final class FooExtension extends CompilerExtension
+{
+
+	public function loadConfiguration(): void
+	{
+		AutoRegistratorExtension::configure(
+			$this->compiler,
+			[
+				'scanDirs' => [__DIR__ . '/..'],
+				'skipDirs' => [
+					'Enum'
+				],
+				'skipFilesPatterns' => [
+					'/Extension\.php$/',
+					'/Event\.php$/',
+				],
+				'skipClasses' => [],
+			]
+		);
+	}
+}
+
+```
---
+++ src/DI/AutoRegistratorExtension.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gamee\AutoRegistrator\DI;
+
+use Nette\DI\Compiler;
+use Nette\DI\CompilerExtension;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * @property-read array $config
+ */
+final class AutoRegistratorExtension extends CompilerExtension
+{
+
+	/**
+	 * @var array
+	 */
+	private $defaults = [
+		'scanDirs' => [],
+		'skipDirs' => [
+			'Exception'
+		],
+		'skipFilesPatterns' => [],
+		'skipClasses' => [],
+	];
+
+
+	public function loadConfiguration(): void
+	{
+		$this->validateConfig($this->defaults);
+
+		foreach ($this->config['scanDirs'] as $dir) {
+			$files = (new Finder)
+				->files()
+				->name('/[A-Z][a-zA-Z0-9]+\.php$/')
+				->notName($this->config['skipFilesPatterns'])
+				->exclude($this->config['skipDirs'])
+				->in($dir);
+
+			foreach ($files as $file) {
+				$handle = fopen((string) $file->getRealPath(), 'r');
+
+				if ($handle === false) {
+					continue;
+				}
+
+				$namespace = null;
+				$className = null;
+
+				while ($line = fgets($handle)) {
+					if (strpos($line, 'namespace ') === 0) {
+						$namespace = substr($line, 10, strlen($line) - 12);
+					} else {
+						$classPosition = strpos($line, 'class ');
+
+						if ($classPosition !== false) {
+							if (strpos($line, 'abstract class') !== false) {
+								break;
+							}
+
+							$className = substr(
+								$line,
+								(6 + $classPosition),
+								strlen($line) - (6 + $classPosition + 1)
+							);
+						}
+					}
+
+					if (($line[0] ?? '') === '{') {
+						break;
+					}
+				}
+
+				fclose($handle);
+
+				if ($className === null || $namespace === null) {
+					continue;
+				}
+
+				$className = explode(' ', $className)[0];
+
+				$fullClassName = $namespace . '\\' . $className;
+
+				if (in_array($fullClassName, $this->config['skipClasses'], true)) {
+					continue;
+				}
+
+				$this->getContainerBuilder()->addDefinition($this->prefix(lcfirst($className)))
+					->setFactory($fullClassName);
+			}
+		}
+	}
+
+
+	public static function configure(
+		Compiler $compiler,
+		array $config
+	): void
+	{
+		$extension = new static;
+		$extension->setCompiler($compiler, 'autoRegistrator');
+		$extension->setConfig($config);
+		$extension->loadConfiguration();
+	}
+}
